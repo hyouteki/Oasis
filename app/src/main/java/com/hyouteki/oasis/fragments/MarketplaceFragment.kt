657b669c6aadd165ee8563e2b4b72935a45a11ad
Lakshay <lakshay21060@iiitd.ac.in>
2023-08-08 22:36:20 +0530

    implemented paging in MarketplaceFragment.kt [referencing]( https://stackoverflow.com/a/50742175/22342267)

--- app/src/main/java/com/hyouteki/oasis/fragments/MarketplaceFragment.kt
+++ app/src/main/java/com/hyouteki/oasis/fragments/MarketplaceFragment.kt
@@ -1,15 +1,14 @@
 package com.hyouteki.oasis.fragments
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter
-import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.Query
 import com.hyouteki.oasis.communicators.MainCommunicator
 import com.hyouteki.oasis.databinding.FragmentMarketplaceBinding
@@ -22,12 +21,12 @@ import com.hyouteki.oasis.viewmodels.OasisViewModel
 class MarketplaceFragment : Fragment() {
     private lateinit var binding: FragmentMarketplaceBinding
     private lateinit var communicator: MainCommunicator
-    private val firebaseQuery = OasisViewModel.postDAO.marketplacePostCollection.orderBy(
-        "postID", Query.Direction.DESCENDING
-    )
-    private lateinit var adapter: MarketplacePostFireStoreAdapter
-    private val recyclerViewOptions = FirestoreRecyclerOptions.Builder<MarketplacePost>()
-        .setQuery(firebaseQuery, MarketplacePost::class.java).build()
+
+    private var collection = OasisViewModel.postDAO.marketplacePostCollection
+    private lateinit var adapter: MarketplacePostAdapter
+    private var lastVisible: DocumentSnapshot? = null
+    private var isScrolling = false
+    private var isLastItemReached = false
 
     companion object {
         const val TAG = "MARKETPLACE_FRAGMENT"
@@ -41,65 +40,72 @@ class MarketplaceFragment : Fragment() {
         binding = FragmentMarketplaceBinding.inflate(inflater, container, false)
         communicator = activity as MainCommunicator
 
-        initialization()
         setupRecyclerView()
 
         return binding.root
     }
 
-    private fun initialization() {
-        class ScrollToTopObserverForMarketplacePost(
-            private val recycler: RecyclerView,
-            private val adapter: MarketplaceFragment.MarketplacePostFireStoreAdapter,
-            private val manager: LinearLayoutManager
-        ) : RecyclerView.AdapterDataObserver() {
-            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
-                super.onItemRangeInserted(positionStart, itemCount)
-                val count = adapter.itemCount
-                val lastVisiblePosition = manager.findLastCompletelyVisibleItemPosition()
-                // If the recycler view is initially being loaded or the
-                // user is at the bottom of the list, scroll to the bottom
-                // of the list to show the newly added message.
-                val loading = lastVisiblePosition == -1
-                val atBottom =
-                    positionStart >= count - 1 && lastVisiblePosition == positionStart - 1
-                if (loading || atBottom) {
-                    recycler.smoothScrollToPosition(positionStart)
+    private fun setupRecyclerView() {
+        collection.orderBy(
+            "postID", Query.Direction.DESCENDING
+        ).limit(PAGING_LIMIT.toLong()).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val dataset = arrayListOf<MarketplacePost>()
+                for (document in task.result) {
+                    dataset.add(document.toObject(MarketplacePost::class.java))
                 }
+                adapter.updateDataset(dataset)
+                lastVisible = task.result.documents[task.result.size() - 1]
+                val onScrollListener: RecyclerView.OnScrollListener =
+                    object : RecyclerView.OnScrollListener() {
+                        override fun onScrollStateChanged(
+                            recyclerView: RecyclerView, newState: Int
+                        ) {
+                            super.onScrollStateChanged(recyclerView, newState)
+                            isScrolling =
+                                isScrolling || newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL
+                        }
+
+                        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                            super.onScrolled(recyclerView, dx, dy)
+                            val linearLayoutManager =
+                                recyclerView.layoutManager as LinearLayoutManager
+                            val firstVisibleItemPosition =
+                                linearLayoutManager.findFirstVisibleItemPosition()
+                            val visibleItemCount = linearLayoutManager.childCount
+                            val totalItemCount = linearLayoutManager.itemCount
+                            if (isScrolling && firstVisibleItemPosition + visibleItemCount == totalItemCount && !isLastItemReached) {
+                                isScrolling = false
+                                val nextQuery: Query = collection.orderBy(
+                                    "postID", Query.Direction.DESCENDING
+                                ).limit(PAGING_LIMIT.toLong())
+                                nextQuery.get().addOnCompleteListener { subTask ->
+                                    if (subTask.isSuccessful) {
+                                        for (subDocument in subTask.result) {
+                                            dataset.add(subDocument.toObject(MarketplacePost::class.java))
+                                        }
+                                        adapter.updateDataset(dataset)
+                                        lastVisible =
+                                            subTask.result.documents[subTask.result.size() - 1]
+                                        isLastItemReached =
+                                            isLastItemReached || subTask.result.size() < PAGING_LIMIT
+                                    }
+                                }
+                            }
+                        }
+                    }
+                binding.recyclerView.addOnScrollListener(onScrollListener)
             }
         }
-        setupRecyclerView()
-        adapter.registerAdapterDataObserver(
-            ScrollToTopObserverForMarketplacePost(
-                binding.recyclerView,
-                adapter,
-                binding.recyclerView.layoutManager as LinearLayoutManager
-            )
-        )
-
-    }
-
-    private fun setupRecyclerView() {
-        adapter = MarketplacePostFireStoreAdapter(recyclerViewOptions)
+        adapter = MarketplacePostAdapter()
         binding.recyclerView.adapter = adapter
     }
 
-    override fun onStart() {
-        super.onStart()
-        setupRecyclerView()
-        adapter.startListening()
-    }
+    inner class MarketplacePostAdapter() :
+        RecyclerView.Adapter<MarketplacePostAdapter.ViewModel>() {
 
-    override fun onStop() {
-        super.onStop()
-        adapter.stopListening()
-    }
+        private val dataset = arrayListOf<MarketplacePost>()
 
-    inner class MarketplacePostFireStoreAdapter(
-        options: FirestoreRecyclerOptions<MarketplacePost>
-    ) : FirestoreRecyclerAdapter<MarketplacePost, MarketplacePostFireStoreAdapter.ViewModel>(
-        options
-    ) {
         inner class ViewModel(marketplacePostListItemBinding: MarketplacePostListItemBinding) :
             RecyclerView.ViewHolder(marketplacePostListItemBinding.root) {
             val userName = marketplacePostListItemBinding.userName
@@ -124,10 +130,8 @@ class MarketplaceFragment : Fragment() {
             )
         }
 
-        @SuppressLint("SetTextI18n")
-        override fun onBindViewHolder(
-            holder: ViewModel, position: Int, model: MarketplacePost
-        ) {
+        override fun onBindViewHolder(holder: ViewModel, position: Int) {
+            val model = dataset[position]
             OasisViewModel.getUserDocument(model.userID).addOnSuccessListener {
                 it.toObject(User::class.java)?.let { user ->
                     holder.userName.text = "@${user.name}"
@@ -159,6 +163,12 @@ class MarketplaceFragment : Fragment() {
             }
         }
 
-        override fun getItemViewType(position: Int) = position
+        override fun getItemCount() = dataset.size
+
+        fun updateDataset(newDataset: List<MarketplacePost>) {
+            dataset.clear()
+            dataset.addAll(newDataset)
+            adapter.notifyDataSetChanged()
+        }
     }
 }
